9799061f7daf3780d97c2c2888882c3d8ae7f966
Kuldeep Sen <kuldeepsen134@gmail.com>
2024-04-20 17:30:05 +0530

    Improve code quality

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,20 +10,19 @@ import Project from "./Project";
 import AppHeader from "../components/AppHeader";
 import HireMe from "./HireMe";
 import Footer from "../components/Footer";
-const Home = () => {
-  const {
-    userData: { data },
-  } = useSelector((state) => state.user);
 
+const Home = () => {
+  const { data } = useSelector((state) => state.user.userData);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(userAbout(""));
   }, [dispatch]);
-  const scrollToAbout = (id) => {
-    const aboutUsSection = document.getElementById(id);
-    if (aboutUsSection) {
-      aboutUsSection.scrollIntoView({ behavior: "smooth" });
+
+  const scrollToSection = (id) => {
+    const section = document.getElementById(id);
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
     }
   };
 
@@ -32,41 +31,35 @@ const Home = () => {
       <AppHeader />
       <div className="hero-section" id="home">
         <div className="container">
-          {data &&
-            data?.map((user, i) => {
-              return (
-                <div className="row align-items-center vh-100 pt-5" key={i}>
-                  <div className="col-lg-7 py-5 ps-lg-5">
-                    <h1>Hi There, I Am {user?.full_name}</h1>
-                    <div>
-                      <p className="fs-3 lh-lg" style={{ color: "#ef7f07" }}>{user?.title} </p >
-                    </div>
-
-                    <div className="d-flex ">
-                        <button
-                          type="button"
-                          className="btn text-white workBtn"
-                          onClick={() => scrollToAbout("ourWork")}
-                        >
-                          View my Works
-                        </button>
-                        <button
-                          type="button"
-                          className="btn text-white workBtn"
-                          onClick={() => scrollToAbout("contactMe")}
-                        >
-                          Contact Me
-                        </button>
-                    </div>
-                  </div>
-                  <div className="col-lg-5">
-                    <div className="avtar-img">
-                      <img src={user?.profile} alt="profile"/>
-                    </div>
-                  </div>
+          {data && data.map((user, i) => (
+            <div className="row align-items-center vh-100 pt-5" key={i}>
+              <div className="col-lg-7 py-5 ps-lg-5">
+                <h1>Hi There, I Am {user?.full_name}</h1>
+                <p className="fs-3 lh-lg" style={{ color: "#ef7f07" }}>{user?.title}</p>
+                <div className="d-flex">
+                  <button
+                    type="button"
+                    className="btn text-white workBtn"
+                    onClick={() => scrollToSection("ourWork")}
+                  >
+                    View my Works
+                  </button>
+                  <button
+                    type="button"
+                    className="btn text-white workBtn"
+                    onClick={() => scrollToSection("contactMe")}
+                  >
+                    Contact Me
+                  </button>
                 </div>
-              );
-            })}
+              </div>
+              <div className="col-lg-5">
+                <div className="avtar-img">
+                  <img src={user?.profile} alt="profile" />
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
       <About />
